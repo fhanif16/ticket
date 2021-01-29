7ff9b5ce836f27bc744edd23f8795d97152ae533
Fatema hanif <fatemahanif16@gmail.com>
2021-01-29 13:32:26 -0500

    first

---
+++ 01 Discover Fly/js.js
@@ -0,0 +1,9 @@
+document.getElementById("plus").addEventListener("click",function(){
+
+   const inputNumber = document.getElementById("input-number");
+   const inputNumberValue = parseInt(inputNumber.value);
+       
+        
+   
+
+})
